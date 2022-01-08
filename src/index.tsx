98d25a3bb3a92a9866b3b89e184da52c67c3c4f0
Tuuby <tobiastrompell@aol.com>
2022-01-08 13:42:58 +0100

    Switch app to SWR for simpler usage

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core';
+import './index.css';
 import App from './App';
-import theme from './theme'
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import configureStore from './Store/store'
-
-const store = configureStore();
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-        <Router>
-          <App />
-        </Router>
-    </ThemeProvider>
-  </Provider>,
-  document.querySelector('#root'),
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
